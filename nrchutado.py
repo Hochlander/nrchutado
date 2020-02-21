2844d7d2fb318d2c15f26b501802826cba9dde0b
Anibal Chaaim <elder.oliveira@ubicuacloud.com.br>
2020-02-21 18:29:54 -0300

    primeiro lote de melhoras

--- nrchutado.py
+++ nrchutado.py
@@ -1,10 +1,14 @@
 import random
 import os
+palpites=[]
 erros=0
-sorteado=random.randrange(0,1000000)
+sorteado=random.randrange(0,100)
 jogador=int(input("Digite seu numero: "))
 while(sorteado!=jogador):
     os.system('cls')
+    print ("palpites que já foram: ") 
+    for i in palpites:
+        print(i)
     if(sorteado>jogador):
         print("ERRO, o numero é maior")
         print(jogador)
@@ -12,5 +16,7 @@ while(sorteado!=jogador):
         print("ERRO, o numero é menor")
         print(jogador)
     erros+=1
+    palpites.append(jogador)
+    
     jogador=int(input("Digite seu numero: "))
     print("Número " + str(jogador) +", você acertou em " +str(erros+1)+ " tentativas")
